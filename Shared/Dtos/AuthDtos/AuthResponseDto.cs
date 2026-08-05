c1509d8a7884c1fb6cc3a86fbf928e432ff16e3f
saadmostafa1174@gmail.com <saadmostafa1174@gmail.com>
2026-08-05 11:01:41 +0300

    feat(security): add authentication and token DTOs

---
+++ Shared/Dtos/AuthDtos/AuthResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos.AuthDtos
+{
+    public class AuthResponseDto
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Token { get; set; }
+        public DateTime TokenExpiresOn { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiresOn { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ Shared/Dtos/AuthDtos/RegisterRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos.AuthDtos
+{
+    public class RegisterRequestDto
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ParentPhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int GradeId { get; set; }
+    }
+}
